3a449f29be3edf8a34433a16622243704e9f00e6
Sukrit Kalra <sukritkalra@gmail.com>
2018-11-07 20:36:06 +0000

    Fixed Docker and installation scripts.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-sudo apt install git python python-pip
-sudo apt install ros-core
-sudo apt install python-rospy    # Installation involves timezone config
-sudo apt install python-catkin-pkg python-cv-bridge
+
+# Install and setup ROS.
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu xenial main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-get update
+sudo apt-get install -y ros-kinetic-ros-base
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+
 # Install pip packages
+sudo apt-get install -y python-pip
 pip install -r requirements.txt
