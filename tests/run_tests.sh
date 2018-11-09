1558ab93a871f4d261208c7b5b8697638ff4e7ce
Yika Luo <yika@yikas-mbp.lan>
2018-11-08 20:35:18 -0800

    can't run all ros tests together

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,10 +4,10 @@
 python tests/communication_1to1_test.py --framework=ros
 python tests/communication_1to2_test.py --framework=ros
 python tests/communication_2to1_test.py --framework=ros
-python control_loop_test.py --framework=ros
+python tests/control_loop_test.py --framework=ros
 
 # Test Ray
 python tests/communication_1to1_test.py --framework=ray
 python tests/communication_1to2_test.py --framework=ray
 python tests/communication_2to1_test.py --framework=ray
-python control_loop_test.py --framework=ray
+python tests/control_loop_test.py --framework=ray
