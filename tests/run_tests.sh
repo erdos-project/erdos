0ec4eb8cf1452eed200571412ef88f9be23ef681
Ionel Gog <gogionel@gmail.com>
2018-11-11 19:57:45 -0800

    Updated Travis config to correctly install dependencies and fail (#15)

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,20 +2,20 @@
 
 # Test ROS
 python tests/communication_pattern_test.py --framework=ros --case=1-1
-if [ $? -ne 0 ] ; then exit $? ; fi
+if [ $? -ne 0 ] ; then exit 1 ; fi
 python tests/communication_pattern_test.py --framework=ros --case=1-2
-if [ $? -ne 0 ] ; then exit $? ; fi
+if [ $? -ne 0 ] ; then exit 1 ; fi
 python tests/communication_pattern_test.py --framework=ros --case=2-1
-if [ $? -ne 0 ] ; then exit $? ; fi
+if [ $? -ne 0 ] ; then exit 1 ; fi
 python tests/control_loop_test.py --framework=ros
-if [ $? -ne 0 ] ; then exit $? ; fi
+if [ $? -ne 0 ] ; then exit 1 ; fi
 
 # Test Ray
 python tests/communication_pattern_test.py --framework=ray --case=1-1
-if [ $? -ne 0 ] ; then exit $? ; fi
+if [ $? -ne 0 ] ; then exit 1 ; fi
 python tests/communication_pattern_test.py --framework=ray --case=1-2
-if [ $? -ne 0 ] ; then exit $? ; fi
+if [ $? -ne 0 ] ; then exit 1 ; fi
 python tests/communication_pattern_test.py --framework=ray --case=2-1
-if [ $? -ne 0 ] ; then exit $? ; fi
+if [ $? -ne 0 ] ; then exit 1 ; fi
 python tests/control_loop_test.py --framework=ray
-if [ $? -ne 0 ] ; then exit $? ; fi
+if [ $? -ne 0 ] ; then exit 1 ; fi
