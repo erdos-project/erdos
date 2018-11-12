5b0a52970026cddbbb92f82b80eaf41f587961ea
Ionel Gog <gogionel@gmail.com>
2018-11-11 19:16:29 -0800

    Ensure run_tests scripts exits with error when a test fails.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,12 +2,20 @@
 
 # Test ROS
 python tests/communication_pattern_test.py --framework=ros --case=1-1
+if [ $? -ne 0 ] ; then exit $? ; fi
 python tests/communication_pattern_test.py --framework=ros --case=1-2
+if [ $? -ne 0 ] ; then exit $? ; fi
 python tests/communication_pattern_test.py --framework=ros --case=2-1
+if [ $? -ne 0 ] ; then exit $? ; fi
 python tests/control_loop_test.py --framework=ros
+if [ $? -ne 0 ] ; then exit $? ; fi
 
 # Test Ray
 python tests/communication_pattern_test.py --framework=ray --case=1-1
+if [ $? -ne 0 ] ; then exit $? ; fi
 python tests/communication_pattern_test.py --framework=ray --case=1-2
+if [ $? -ne 0 ] ; then exit $? ; fi
 python tests/communication_pattern_test.py --framework=ray --case=2-1
+if [ $? -ne 0 ] ; then exit $? ; fi
 python tests/control_loop_test.py --framework=ray
+if [ $? -ne 0 ] ; then exit $? ; fi
