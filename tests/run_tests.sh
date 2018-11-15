fda4b1f14666349fe808b4d8c7b1a7dd84b5c9fd
Yika Luo <yika@calvisitor-10-105-137-102.calvisitor.1918.berkeley.edu>
2018-11-14 17:49:40 -0800

    add deadline test to integration test

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -9,6 +9,8 @@ python tests/communication_pattern_test.py --framework=ros --case=2-1
 if [ $? -ne 0 ] ; then exit 1 ; fi
 python tests/control_loop_test.py --framework=ros
 if [ $? -ne 0 ] ; then exit 1 ; fi
+python tests/deadline_test.py --framework=ros
+if [ $? -ne 0 ] ; then exit 1 ; fi
 
 # Test Ray
 python tests/communication_pattern_test.py --framework=ray --case=1-1
@@ -18,4 +20,6 @@ if [ $? -ne 0 ] ; then exit 1 ; fi
 python tests/communication_pattern_test.py --framework=ray --case=2-1
 if [ $? -ne 0 ] ; then exit 1 ; fi
 python tests/control_loop_test.py --framework=ray
+if [ $? -ne 0 ] ; then exit 1 ; fi
+python tests/deadline_test.py --framework=ray
 if [ $? -ne 0 ] ; then exit 1 ; fi
