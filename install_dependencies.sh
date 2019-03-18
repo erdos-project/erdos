550a8fc4cb7b914a81cf999788fa6be3d5e48278
Sukrit Kalra <sukritkalra@gmail.com>
2019-03-18 17:50:40 +0000

    [FIX] Fixing TravisCI build.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,7 +4,7 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu xenial main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
-sudo apt-get install -y ros-kinetic-ros-base
+sudo apt-get install -y --allow-unauthenticated ros-kinetic-ros-base
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 
 # Install pip packages
