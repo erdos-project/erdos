fc59d2d4f9f7bc41bb72271c2eb45c7a0fd93acd
Ionel Gog <gogionel@gmail.com>
2019-04-02 18:07:13 -0700

    Updated dependency fetcher script for the Pylot example pipeline.

--- examples/pylot/dependencies/get_models.sh
+++ examples/pylot/dependencies/get_models.sh
@@ -5,3 +5,11 @@ cd data
 wget https://www.dropbox.com/s/i6v54gng0rao6ff/drn_d_22_cityscapes.pth
 wget https://pjreddie.com/media/files/yolov3.weights
 wget https://www.dropbox.com/s/fgvvfsjuezbswy2/traffic_light_det_inference_graph.pb
+wget http://download.tensorflow.org/models/object_detection/faster_rcnn_resnet101_coco_2018_01_28.tar.gz
+tar -xvf faster_rcnn_resnet101_coco_2018_01_28.tar.gz
+wget http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_coco_2018_01_28.tar.gz
+tar -xvf ssd_mobilenet_v1_coco_2018_01_28.tar.gz
+pip install gdown
+gdown http://drive.google.com/uc?id=0B1sg8Yyw1JCDOUNsYkpQTGdLYVU
+mkdir conv_reg_vot/vgg_model
+mv VGG_16_layers_py3.npz conv_reg_vot/vgg_model/
