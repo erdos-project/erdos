906ae29cebb936020eb2432ada6f0f512234f003
Ionel Gog <gogionel@gmail.com>
2019-05-18 18:24:15 -0700

    [ERDOS] Removed superfluous op field from ROSOutputDataStream.

--- erdos/ros/ros_executor.py
+++ erdos/ros/ros_executor.py
@@ -52,7 +52,7 @@ class ROSExecutor(Executor):
             for input_stream in self.op_handle.input_streams
         ]
         ros_output_streams = [
-            ROSOutputDataStream(op, output_stream)
+            ROSOutputDataStream(output_stream)
             for output_stream in self.op_handle.output_streams
         ]
         op._add_input_streams(ros_input_streams)
