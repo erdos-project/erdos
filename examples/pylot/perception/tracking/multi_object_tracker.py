3ec411e488031528c00103e710b8e29ccecfcd73
Ionel Gog <gogionel@gmail.com>
2019-06-04 15:01:56 -0700

    Removed Pylot. Code moved to https://github.com/erdos-project/pylot

--- examples/pylot/perception/tracking/multi_object_tracker.py
+++
@@ -1,9 +0,0 @@
-class MultiObjectTracker(object):
-    def __init__(self):
-        self._trackers = []
-
-    def reinitialize(self, frame, bboxes):
-        pass
-
-    def track(self, frame):
-        return True, [tracker.track(frame) for tracker in self._trackers]
