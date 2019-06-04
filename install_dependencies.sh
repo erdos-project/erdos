445ddaf21e3ba6ca3b4168f19063d09298676b90
Ionel Gog <gogionel@gmail.com>
2019-06-04 14:49:31 -0700

    Updated install script to differentiate between Ubuntu versions.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,13 +1,40 @@
 #!/bin/bash
 
-# Install and setup ROS.
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu xenial main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-sudo apt-get update
-sudo apt-get install -y --allow-unauthenticated ros-kinetic-ros-base
-echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
-sudo apt install python-cv-bridge
+os_version=$(lsb_release -sr)
+
+###############################################################################
+# Install ROS
+###############################################################################
+if [ $os_version == '16.04' ]
+then
+    # Install and setup ROS.
+    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu xenial main" > /etc/apt/sources.list.d/ros-latest.list'
+    sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+    sudo apt-get update
+    sudo apt-get install -y --allow-unauthenticated ros-kinetic-ros-base
+    echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+elif [ $os_version == '18.04' ]
+then
+    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu bionic main" > /etc/apt/sources.list.d/ros-latest.list'
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+    sudo apt-get update
+    sudo apt-get install -y --allow-unauthenticated ros-melodic-ros-base
+    echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+else
+    echo "Unsupported OS version"
+    exit 1
+
+sudo apt-get install -y python-cv-bridge python-rosinstall
+
+# Initialize rosdep
+sudo rosdep init
+rosdep update
+
+###############################################################################
+# Install ERDOS
+###############################################################################
 
 # Install pip packages
 sudo apt-get install -y python-pip
+
 pip install erdos
--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = "erdos",
-    version = "0.1",
-    packages = find_packages(exclude = ("examples*", "tests*",)),
-    license = "Apache 2.0",
-    url = "https://github.com/erdos-project/erdos",
-    long_description = open("README.md").read(),
-    install_requires = [
+    name="erdos",
+    version="0.1.1",
+    author="ERDOS Team",
+    description=("A platform for developing robots and autonomous vehicles."),
+    long_description=open("README.md").read(),
+    url="https://github.com/erdos-project/erdos",
+    packages=find_packages(exclude=("examples*", "tests*",)),
+    license="Apache 2.0",
+    install_requires=[
         "absl-py",
         "ray",
         "rospkg",
+        "setproctitle",
     ],
 )
-
