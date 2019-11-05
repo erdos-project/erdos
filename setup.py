50f5881f222ed7e7f9c4ccfd79fddb29e38dc84f
Ionel Gog <gogionel@gmail.com>
2019-11-05 09:25:05 -0800

    Updated to release version 0.1.3.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="erdos",
-    version="0.1.2",
+    version="0.1.3",
     author="ERDOS Team",
     description=("A platform for developing robots and autonomous vehicles."),
     long_description=open("README.md").read(),
