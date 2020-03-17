6daf48e7ea102f11916917a49b3d2cbbc1cb6007
jasminevle <jasminele@berkeley.edu>
2020-03-16 22:55:28 -0700

    [Python] Added window and map operators to operators submodule (#93)

---
+++ python/erdos/operators/map.py
@@ -0,0 +1,13 @@
+class Map(erdos.Operator):
+    """Applies the provided function to a message and sends the resulting message."""
+    def __init__(self, read_stream, write_stream, function):
+        read_stream.add_callback(self.callback, [write_stream])
+        self.function = function
+
+    def callback(self, msg, write_stream):
+        msg = self.function(msg)
+        write_stream.send(msg)
+
+    @staticmethod
+    def connect(read_streams):
+        return [erdos.WriteStream()]
