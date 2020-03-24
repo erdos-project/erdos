9dcde096c77a48c144deb3455ca4165e4739d8c6
jasminevle <jasminele@berkeley.edu>
2020-03-24 13:45:42 -0700

    [Python] Added window map example, modified import statements in submodule (#99)

--- python/erdos/operators/map.py
+++ python/erdos/operators/map.py
@@ -1,3 +1,6 @@
+import erdos
+
+
 class Map(erdos.Operator):
     """Applies the provided function to a message and sends the resulting message."""
     def __init__(self, read_stream, write_stream, function):
