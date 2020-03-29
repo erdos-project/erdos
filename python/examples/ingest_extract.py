f8ffa06ef96dc729412a57d8af29bceca612023a
Peter Schafhalter <pschafhalter@berkeley.edu>
2020-03-29 15:52:36 -0700

    Replace operator with Map

--- python/examples/ingest_extract.py
+++ python/examples/ingest_extract.py
@@ -3,28 +3,23 @@
 2) An operator squares the number.
 3) The python script receives the result.
 """
-import erdos
 import time
 
+import erdos
+from erdos.operators.map import Map
 
-class SquareOp(erdos.Operator):
-    def __init__(self, read_stream, write_stream):
-        read_stream.add_callback(self.callback, [write_stream])
 
-    def callback(self, msg, write_stream):
-        print("SquareOp: received {msg}".format(msg=msg))
-        msg = erdos.Message(msg.timestamp, msg.data * msg.data)
-        write_stream.send(msg)
-
-    @staticmethod
-    def connect(read_streams):
-        return [erdos.WriteStream()]
+def square_msg(msg):
+    """Squares the data from an ERDOS message."""
+    print("SquareOp: received {msg}".format(msg=msg))
+    return erdos.Message(msg.timestamp, msg.data * msg.data)
 
 
 def main():
     ingest_stream = erdos.IngestStream()
-    (square_stream, ) = erdos.connect(SquareOp, erdos.OperatorConfig(),
-                                      [ingest_stream])
+    (square_stream, ) = erdos.connect(Map,
+                                      erdos.OperatorConfig(), [ingest_stream],
+                                      function=square_msg)
     extract_stream = erdos.ExtractStream(square_stream)
 
     erdos.run_async()
@@ -35,7 +30,6 @@ def main():
         send_msg = erdos.Message(timestamp, count)
         print("IngestStream: sending {send_msg}".format(send_msg=send_msg))
         ingest_stream.send(send_msg)
-        ingest_stream.send(erdos.WatermarkMessage(timestamp))
         recv_msg = extract_stream.read()
         print("ExtractStream: received {recv_msg}".format(recv_msg=recv_msg))
 
