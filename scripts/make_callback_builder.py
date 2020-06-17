31173a9d43001e395a6c6133b94bde4534e93d96
Ionel Gog <gogionel@gmail.com>
2020-06-16 18:44:09 -0700

    Extended macro builder to support operators with more input streams.

--- scripts/make_callback_builder.py
+++ scripts/make_callback_builder.py
@@ -35,6 +35,13 @@ num_to_str = {
     6: "Six",
     7: "Seven",
     8: "Eight",
+    9: "Nine",
+    10: "Ten",
+    11: "Eleven",
+    12: "Twelve",
+    13: "Thirteen",
+    14: "Fourteen",
+    15: "Fifteen",
 }
 
 
