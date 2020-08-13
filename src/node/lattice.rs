5b8f7361ed76951dd77aa0647abdf73ed5a87a36
Peter Schafhalter <pschafhalter@berkeley.edu>
2020-08-13 11:53:19 -0700

    Use TERMINAL_LOGGER

--- src/node/lattice.rs
+++ src/node/lattice.rs
@@ -319,7 +319,7 @@ impl ExecutionLattice {
 
         if forest.node_count() > 100 {
             slog::warn!(
-                crate::get_terminal_logger(),
+                crate::TERMINAL_LOGGER,
                 "{} operator events queued in lattice. Increase number of operator executors or decrease incoming \
                 message frequency to reduce load.", forest.node_count()
             )
