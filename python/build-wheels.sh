5cd3aebbbb08990e57b2ee81ccccf4861c88efbf
Peter Schafhalter <pschafhalter@berkeley.edu>
2020-09-24 23:32:25 -0700

    Enable nightly builds for Python wheels (#144)

--- python/build-wheels.sh
+++ python/build-wheels.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 set -ex
 
-curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly -y
-export PATH="$HOME/.cargo/bin:$PATH"
-rustup default nightly-2020-06-22
+curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly-2020-06-22 --profile minimal -y
+source $HOME/.cargo/env
 
-cd /io
+if [ "$1" == github-actions ]; then
+    cd /github/workspace/
+else
+    cd /io
+fi
 
 for PYBIN in /opt/python/{cp35-cp35m,cp36-cp36m,cp37-cp37m,cp38-cp38}/bin; do
-    export PATH_BACKUP=$PATH
-    PATH="$PYBIN:$PATH"
-    # Require wheel==0.31.1 because auditwheel breaks on newer versions
-    "${PYBIN}/pip" install -U setuptools wheel==0.31.1 setuptools-rust
+    "${PYBIN}/pip" install -U setuptools wheel setuptools-rust
     "${PYBIN}/python" python/setup.py bdist_wheel
-    PATH=$PATH_BACKUP
 done
 
+# Update auditwheel on python3.8 to avoid errors.
+/opt/python/cp38-cp38/bin/pip install -U auditwheel
+
 for whl in dist/*.whl; do
-    auditwheel repair "$whl" -w dist/
+    /opt/python/cp38-cp38/bin/python -m auditwheel repair "$whl" -w dist/
 done
