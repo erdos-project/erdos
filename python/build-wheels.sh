dd7873e6d8d8d8dda9146523bc96012bd216f7cb
Peter Schafhalter <pschafhalter@berkeley.edu>
2020-09-25 02:18:10 -0700

    [BUG] Fix bug in nightly build

--- python/build-wheels.sh
+++ python/build-wheels.sh
@@ -11,13 +11,10 @@ else
 fi
 
 for PYBIN in /opt/python/{cp35-cp35m,cp36-cp36m,cp37-cp37m,cp38-cp38}/bin; do
-    # Temporarily add the python directory to the path so the ERDOS build doesn't
-    # error when it calls python3 in build.rs.
-    PREV_PATH=$PATH
-    PATH="$PYBIN:$PATH"
     "${PYBIN}/pip" install -U setuptools wheel setuptools-rust
-    "${PYBIN}/python" python/setup.py bdist_wheel
-    PATH=$PREV_PATH
+    # Add the python directory to the path so the ERDOS build doesn't
+    # error when it invokes python3 in build.rs.
+    PATH="$PYBIN:$PATH" "${PYBIN}/python" python/setup.py bdist_wheel
 done
 
 # Update auditwheel on python3.8 to avoid errors.
