3187eba9df2945d72173ab5ae8f0fc10158ae06e
Sukrit Kalra <sukritkalra@gmail.com>
2021-07-11 17:11:30 -0700

    Clean up connect

--- src/dataflow/connect.rs
+++ src/dataflow/connect.rs
@@ -157,7 +157,7 @@ pub fn connect_sink<O, S, T>(
     );
 }
 
-pub fn connect_parallel_only_one_in_one_out<O, S, T, U, V>(
+pub fn connect_parallel_one_in_one_out<O, S, T, U, V>(
     operator_fn: impl Fn() -> O + Clone + Send + Sync + 'static,
     state_fn: impl Fn() -> S + Clone + Send + Sync + 'static,
     mut config: OperatorConfig,
@@ -272,63 +272,6 @@ where
 
     write_stream
 }
-
-/*
-pub fn connect_one_in_one_out<O, S, T, U>(
-    operator_fn: impl Fn() -> O + Clone + Send + Sync + 'static,
-    state_fn: impl Fn() -> S + Clone + Send + Sync + 'static,
-    mut config: OperatorConfig,
-    read_stream: &impl StreamT<T>,
-) -> Stream<U>
-where
-    O: 'static + OneInOneOut<S, T, U>,
-    S: State,
-    T: Data + for<'a> Deserialize<'a>,
-    U: Data + for<'a> Deserialize<'a>,
-{
-    config.id = OperatorId::new_deterministic();
-    let write_stream = Stream::new();
-
-    let read_stream_ids = vec![read_stream.id()];
-    let write_stream_ids = vec![write_stream.id()];
-
-    let read_stream_ids_copy = read_stream_ids.clone();
-    let write_stream_ids_copy = write_stream_ids.clone();
-    let config_copy = config.clone();
-    let op_runner =
-        move |channel_manager: Arc<Mutex<ChannelManager>>| -> Box<dyn OperatorExecutorT> {
-            let mut channel_manager = channel_manager.lock().unwrap();
-
-            let read_stream = channel_manager
-                .take_read_stream(read_stream_ids_copy[0])
-                .unwrap();
-            let write_stream = channel_manager
-                .get_write_stream(write_stream_ids_copy[0])
-                .unwrap();
-
-            let executor = OneInOneOutExecutor::new(
-                config_copy.clone(),
-                operator_fn.clone(),
-                state_fn.clone(),
-                read_stream,
-                write_stream,
-            );
-
-            Box::new(executor)
-        };
-
-    default_graph::add_operator(
-        config.id,
-        config.name,
-        config.node_id,
-        read_stream_ids,
-        write_stream_ids,
-        op_runner,
-    );
-    default_graph::add_operator_stream(config.id, &write_stream);
-
-    write_stream
-}*/
 
 pub fn connect_parallel_two_in_one_out<O, S, T, U, V, W>(
     operator_fn: impl Fn() -> O + Clone + Send + Sync + 'static,
@@ -456,69 +399,6 @@ where
 
     write_stream
 }
-
-/*
-pub fn connect_two_in_one_out<O, S, T, U, V>(
-    operator_fn: impl Fn() -> O + Clone + Send + Sync + 'static,
-    state_fn: impl Fn() -> S + Clone + Send + Sync + 'static,
-    mut config: OperatorConfig,
-    left_read_stream: &impl StreamT<T>,
-    right_read_stream: &impl StreamT<U>,
-) -> Stream<V>
-where
-    O: 'static + TwoInOneOut<S, T, U, V>,
-    S: State,
-    T: Data + for<'a> Deserialize<'a>,
-    U: Data + for<'a> Deserialize<'a>,
-    V: Data + for<'a> Deserialize<'a>,
-{
-    config.id = OperatorId::new_deterministic();
-    let write_stream = Stream::new();
-
-    let read_stream_ids = vec![left_read_stream.id(), right_read_stream.id()];
-    let write_stream_ids = vec![write_stream.id()];
-
-    let read_stream_ids_copy = read_stream_ids.clone();
-    let write_stream_ids_copy = write_stream_ids.clone();
-    let config_copy = config.clone();
-    let op_runner =
-        move |channel_manager: Arc<Mutex<ChannelManager>>| -> Box<dyn OperatorExecutorT> {
-            let mut channel_manager = channel_manager.lock().unwrap();
-
-            let left_read_stream = channel_manager
-                .take_read_stream(read_stream_ids_copy[0])
-                .unwrap();
-            let right_read_stream = channel_manager
-                .take_read_stream(read_stream_ids_copy[1])
-                .unwrap();
-            let write_stream = channel_manager
-                .get_write_stream(write_stream_ids_copy[0])
-                .unwrap();
-
-            let executor = TwoInOneOutExecutor::new(
-                config_copy.clone(),
-                operator_fn.clone(),
-                state_fn.clone(),
-                left_read_stream,
-                right_read_stream,
-                write_stream,
-            );
-
-            Box::new(executor)
-        };
-
-    default_graph::add_operator(
-        config.id,
-        config.name,
-        config.node_id,
-        read_stream_ids,
-        write_stream_ids,
-        op_runner,
-    );
-    default_graph::add_operator_stream(config.id, &write_stream);
-
-    write_stream
-}*/
 
 pub fn connect_parallel_one_in_two_out<O, S, T, U, V, W>(
     operator_fn: impl Fn() -> O + Clone + Send + Sync + 'static,
