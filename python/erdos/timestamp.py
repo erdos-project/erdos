7b32d2d93940e4eb7bfcc51326428368904505bb
Sukrit Kalra <sukritkalra@gmail.com>
2021-08-30 15:13:53 -0700

    Move Timestamp's dunder methods to Rust.

--- python/erdos/timestamp.py
+++ python/erdos/timestamp.py
@@ -13,11 +13,6 @@ class Timestamp(object):
         to the message or watermark.
     - Timestamp::Top: The final timestamp, and used for destruction of
         operators.
-
-   Attributes:
-       coordinates (`Sequence[int]`): An array of coordinates.
-       is_bottom (`bool`): Representing Timestamp::Bottom.
-       is_top (`bool`): Representing Timestamp::Top.
     """
     def __init__(self,
                  timestamp=None,
@@ -27,27 +22,20 @@ class Timestamp(object):
                  _py_timestamp: PyTimestamp = None):
         if _py_timestamp is not None:
             # Initialize from PyTimestamp, if available.
-            self._is_top = _py_timestamp.is_top
-            self._is_bottom = _py_timestamp.is_bottom
-            self.coordinates = _py_timestamp.coordinates
+            self._py_timestamp = _py_timestamp
         elif timestamp is not None:
             # If Timestamp is available, copy its contents.
-            self._is_top = timestamp.is_top
-            self._is_bottom = timestamp.is_bottom
-            self.coordinates = timestamp.coordinates
+            self._py_timestamp = timestamp._py_timestamp
         else:
             if is_top and not is_bottom and coordinates is None:
-                self._is_top = is_top
-                self._is_bottom = is_bottom
-                self.coordinates = coordinates
+                self._py_timestamp = PyTimestamp(coordinates,
+                                                 is_top, is_bottom)
             elif is_bottom and not is_top and coordinates is None:
-                self._is_top = is_top
-                self._is_bottom = is_bottom
-                self.coordinates = coordinates
+                self._py_timestamp = PyTimestamp(coordinates,
+                                                 is_top, is_bottom)
             elif coordinates is not None and not is_bottom and not is_top:
-                self._is_top = is_top
-                self._is_bottom = is_bottom
-                self.coordinates = coordinates
+                self._py_timestamp = PyTimestamp(coordinates,
+                                                 is_top, is_bottom)
             else:
                 raise ValueError("Timestamp should either have coordinates"
                                  "or be either Top or Bottom")
@@ -58,99 +46,42 @@ class Timestamp(object):
         Returns:
             The :py:class:`PyTimestamp` instance representing self.
         """
-        return PyTimestamp(self.coordinates, self.is_top, self.is_bottom)
+        return self._py_timestamp
 
     def __repr__(self):
-        if self.is_top:
-            return "Timestamp::Top"
-        elif self.is_bottom:
-            return "Timestamp::Bottom"
-        else:
-            return "Timestamp::Time({})".format(str(self.coordinates))
+        return str(self._py_timestamp)
 
     def __str__(self):
-        return self.__repr__()
+        return repr(self._py_timestamp)
 
     def __eq__(self, timestamp):
-        if self.is_top and timestamp.is_top:
-            return True
-        if self.is_bottom and timestamp.is_bottom:
-            return True
-        if len(self.coordinates) != len(timestamp.coordinates):
-            return False
-        for coord, other_coord in zip(self.coordinates, timestamp.coordinates):
-            if coord != other_coord:
-                return False
-        return True
+        return self._py_timestamp == timestamp._py_timestamp
 
     def __ne__(self, timestamp):
-        return not self.__eq__(timestamp)
+        return self._py_timestamp != timestamp._py_timestamp
 
     def __lt__(self, timestamp):
-        # Compare Bottom timestamps.
-        if self.is_bottom and timestamp.is_bottom:
-            return False
-        if self.is_bottom and not timestamp.is_bottom:
-            return True
-        if not self.is_bottom and timestamp.is_bottom:
-            return False
-
-        # Compare Top timestamps.
-        if self.is_top:
-            return False
-        if not self.is_top and timestamp.is_top:
-            return True
-
-        # Compare coordinates.
-        if len(self.coordinates) != len(timestamp.coordinates):
-            raise Exception(
-                "Cannot compare timestamps of different size {} and {}".format(
-                    self, timestamp))
-        for coord, other_coord in zip(self.coordinates, timestamp.coordinates):
-            if coord > other_coord:
-                return False
-            elif coord < other_coord:
-                return True
-        return False
+        return self._py_timestamp < timestamp._py_timestamp
 
     def __le__(self, timestamp):
-        # Compare Bottom timestamps.
-        if self.is_bottom:
-            return True
-        if not self.is_bottom and timestamp.is_bottom:
-            return False
-
-        # Compare Top timestamps.
-        if self.is_top:
-            return timestamp.is_top
-        if not self.is_top and timestamp.is_top:
-            return True
-
-        # Compare coordinates.
-        if len(self.coordinates) != len(timestamp.coordinates):
-            raise Exception(
-                "Cannot compare timestamps of different size {} and {}".format(
-                    self, timestamp))
-        for coord, other_coord in zip(self.coordinates, timestamp.coordinates):
-            if coord > other_coord:
-                return False
-            elif coord < other_coord:
-                return True
-        return True
+        return self._py_timestamp <= timestamp._py_timestamp
 
     def __gt__(self, timestamp):
-        return not self.__le__(timestamp)
+        return self._py_timestamp > timestamp._py_timestamp
 
     def __ge__(self, timestamp):
-        return not self.__lt__(timestamp)
+        return self._py_timestamp >= timestamp._py_timestamp
 
     def __hash__(self):
-        return hash((tuple(self.coordinates), self.is_top, self.is_bottom))
+        coordinates = self._py_timestamp.coordinates()
+        if coordinates is not None:
+            coordinates = tuple(coordinates)
+        return hash((coordinates, self.is_top, self.is_bottom))
 
     @property
     def is_top(self):
-        return self._is_top
+        return self._py_timestamp.is_top()
 
     @property
     def is_bottom(self):
-        return self._is_bottom
+        return self._py_timestamp.is_bottom()
--- src/python/py_timestamp.rs
+++ src/python/py_timestamp.rs
@@ -1,5 +1,5 @@
 use crate::dataflow::Timestamp;
-use pyo3::{exceptions, prelude::*};
+use pyo3::{basic::CompareOp, exceptions, prelude::*, PyObjectProtocol};
 
 /// A Python version of ERDOS' Timestamp.
 ///
@@ -8,12 +8,7 @@ use pyo3::{exceptions, prelude::*};
 #[pyclass]
 #[derive(Clone)]
 pub(crate) struct PyTimestamp {
-    #[pyo3(get)]
-    coordinates: Option<Vec<u64>>,
-    #[pyo3(get)]
-    is_top: bool,
-    #[pyo3(get)]
-    is_bottom: bool,
+    timestamp: Timestamp,
 }
 
 #[pymethods]
@@ -22,57 +17,70 @@ impl PyTimestamp {
     fn new(coordinates: Option<Vec<u64>>, is_top: bool, is_bottom: bool) -> PyResult<Self> {
         match (coordinates, is_top, is_bottom) {
             (None, true, false) => Ok(Self {
-                coordinates: None,
-                is_top,
-                is_bottom,
+                timestamp: Timestamp::Top,
             }),
             (None, false, true) => Ok(Self {
-                coordinates: None,
-                is_top,
-                is_bottom,
+                timestamp: Timestamp::Bottom,
             }),
             (Some(c), false, false) => Ok(Self {
-                coordinates: Some(c),
-                is_top,
-                is_bottom,
+                timestamp: Timestamp::Time(c),
             }),
             (_, _, _) => Err(exceptions::PyValueError::new_err(
                 "Timestamp should either have coordinates or be either Top or Bottom.",
             )),
         }
     }
+
+    fn is_top(&self) -> bool {
+        self.timestamp.is_top()
+    }
+
+    fn is_bottom(&self) -> bool {
+        self.timestamp.is_bottom()
+    }
+
+    fn coordinates(&self) -> Option<Vec<u64>> {
+        match &self.timestamp {
+            Timestamp::Time(c) => Some(c.clone()),
+            _ => None,
+        }
+    }
+}
+
+#[pyproto]
+impl PyObjectProtocol for PyTimestamp {
+    fn __str__(&self) -> PyResult<String> {
+        match &self.timestamp {
+            Timestamp::Top => Ok(String::from("Timestamp::Top")),
+            Timestamp::Bottom => Ok(String::from("Timestamp::Bottom")),
+            Timestamp::Time(c) => Ok(format!("Timestamp::Time({:?})", c)),
+        }
+    }
+
+    fn __repr__(&self) -> PyResult<String> {
+        self.__str__()
+    }
+
+    fn __richcmp__(&self, other: PyTimestamp, op: CompareOp) -> PyResult<bool> {
+        match op {
+            CompareOp::Lt => Ok(self.timestamp < other.timestamp),
+            CompareOp::Le => Ok(self.timestamp <= other.timestamp),
+            CompareOp::Eq => Ok(self.timestamp == other.timestamp),
+            CompareOp::Ne => Ok(self.timestamp != other.timestamp),
+            CompareOp::Gt => Ok(self.timestamp > other.timestamp),
+            CompareOp::Ge => Ok(self.timestamp >= other.timestamp),
+        }
+    }
 }
 
 impl From<Timestamp> for PyTimestamp {
     fn from(timestamp: Timestamp) -> Self {
-        match timestamp {
-            Timestamp::Bottom => Self {
-                coordinates: None,
-                is_top: false,
-                is_bottom: true,
-            },
-            Timestamp::Time(c) => Self {
-                coordinates: Some(c),
-                is_top: false,
-                is_bottom: false,
-            },
-            Timestamp::Top => Self {
-                coordinates: None,
-                is_top: true,
-                is_bottom: false,
-            },
-        }
+        Self { timestamp }
     }
 }
 
 impl From<PyTimestamp> for Timestamp {
     fn from(timestamp: PyTimestamp) -> Self {
-        if timestamp.is_bottom {
-            return Timestamp::Bottom;
-        } else if timestamp.is_top {
-            return Timestamp::Top;
-        } else {
-            return Timestamp::Time(timestamp.coordinates.unwrap());
-        }
+        timestamp.timestamp.clone()
     }
 }
