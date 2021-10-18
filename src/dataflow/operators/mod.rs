3a0be5c898b36d4ab0c2130056068051832b7bd3
Sriharsha Guduguntla <sguduguntla@users.noreply.github.com>
2021-10-18 13:57:52 -0700

    Finished new redesigned MapOperator, FilterOperator, and SplitOperator (#170)
    * Completed new Split, Filter, and Map Operators
    
    * Apply suggestions from @pschafhalter's code review
    
    Co-authored-by: Peter Schafhalter <pschafhalter@berkeley.edu>
    
    Co-authored-by: Sukrit Kalra <sukritkalra@gmail.com>
    Co-authored-by: Peter Schafhalter <pschafhalter@berkeley.edu>

--- src/dataflow/operators/mod.rs
+++ src/dataflow/operators/mod.rs
@@ -1,11 +1,13 @@
 //! Library of generic operators for building ERDOS applications.
 
 // Private submodules
-mod join_operator;
+// mod join_operator;
+mod filter_operator;
 mod map_operator;
-mod source_operator;
+mod split_operator;
 
 // Public exports
-pub use crate::dataflow::operators::join_operator::JoinOperator;
+// pub use crate::dataflow::operators::join_operator::JoinOperator;
+pub use crate::dataflow::operators::filter_operator::FilterOperator;
 pub use crate::dataflow::operators::map_operator::MapOperator;
-pub use crate::dataflow::operators::source_operator::SourceOperator;
+pub use crate::dataflow::operators::split_operator::SplitOperator;
