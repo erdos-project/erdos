0e7ac3c93b48e9933cb69619ea730411c0cb364b
Peter Schafhalter <pschafhalter@berkeley.edu>
2021-10-18 14:58:40 -0700

    Remove num_event_runners from OperatorConfig

--- src/dataflow/operator.rs
+++ src/dataflow/operator.rs
@@ -305,14 +305,14 @@ where
 
 #[derive(Clone)]
 pub struct OperatorConfig {
-    /// A human-readable name for the [`Operator`] used in logging.
+    /// A human-readable name for the [operator](self) used in logging.
     pub name: Option<String>,
-    /// A unique identifier for the [`Operator`].
+    /// A unique identifier for the [operator](self).
     /// ERDOS sets this value when the dataflow graph executes.
     /// Currently the ID is inaccessible from the driver, but is set when the config
     /// is passed to the operator.
     pub id: OperatorId,
-    /// Whether the [`Operator`] should automatically send
+    /// Whether the [operator](self) should automatically send
     /// [watermark messages](crate::dataflow::Message::Watermark) on all
     /// [`WriteStream`](crate::dataflow::WriteStream)s for
     /// [`Timestamp`](crate::dataflow::Timestamp) `t` upon receiving watermarks with
@@ -324,10 +324,6 @@ pub struct OperatorConfig {
     pub flow_watermarks: bool,
     /// The ID of the node on which the operator should run. Defaults to `0`.
     pub node_id: NodeId,
-    /// Number of parallel tasks which process callbacks.
-    /// A higher number may result in more parallelism; however this may be limited
-    /// by dependencies on [`State`](crate::dataflow::State) and timestamps.
-    pub num_event_runners: usize,
 }
 
 impl OperatorConfig {
@@ -337,36 +333,24 @@ impl OperatorConfig {
             name: None,
             flow_watermarks: true,
             node_id: 0,
-            num_event_runners: 1,
         }
     }
 
-    /// Set the [`Operator`]'s name.
+    /// Set the [operator](self)'s name.
     pub fn name(mut self, name: &str) -> Self {
         self.name = Some(name.to_string());
         self
     }
 
-    /// Set whether the [`Operator`] should flow watermarks.
+    /// Set whether the [operator](self) should flow watermarks.
     pub fn flow_watermarks(mut self, flow_watermarks: bool) -> Self {
         self.flow_watermarks = flow_watermarks;
         self
     }
 
-    /// Set the node on which the [`Operator`] runs.
+    /// Set the node on which the [operator](self) runs.
     pub fn node(mut self, node_id: NodeId) -> Self {
         self.node_id = node_id;
-        self
-    }
-
-    /// Sets the maximum number of callbacks the operator can process in parallel
-    /// at a time. Defaults to 1.
-    pub fn num_event_runners(mut self, num_event_runners: usize) -> Self {
-        assert!(
-            num_event_runners > 0,
-            "Operator must have at least 1 thread."
-        );
-        self.num_event_runners = num_event_runners;
         self
     }
 
