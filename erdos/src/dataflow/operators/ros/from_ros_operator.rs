1ba2570381b8b42442a7c45fcfa69c789fce4026
Sukrit Kalra <sukritkalra@gmail.com>
2022-02-01 12:26:58 -0800

    Fix ROS doctests

--- erdos/src/dataflow/operators/ros/from_ros_operator.rs
+++ erdos/src/dataflow/operators/ros/from_ros_operator.rs
@@ -55,7 +55,6 @@ use std::sync::{Arc, Mutex};
 ///     move || -> FromRosOperator<rosrust_msg::sensor_msgs::Image, Vec<u8>> {
 ///         FromRosOperator::new("image_topic", ros_image_to_bytes)
 ///     },
-///     || {},
 ///     ros_source_config,
 /// );
 /// ```
