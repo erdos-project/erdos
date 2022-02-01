ff42f6faa8c4b58bcc1d1cfa51b4438a4e6d3258
Sukrit Kalra <sukritkalra@gmail.com>
2022-02-01 11:54:27 -0800

    Implement TimeVersionedState for LINQ examples.

--- examples/linq.rs
+++ examples/linq.rs
@@ -1,12 +1,13 @@
-use std::{collections::HashMap, thread, time::Duration};
+use std::{thread, time::Duration};
 
 use erdos::{
     dataflow::{
         context::SinkContext,
         operator::{Sink, Source},
         operators::{Filter, Map, Split},
+        state::TimeVersionedState,
         stream::{WriteStream, WriteStreamT},
-        Message, OperatorConfig, StateT, Timestamp,
+        Message, OperatorConfig, Timestamp,
     },
     node::Node,
     Configuration,
@@ -20,7 +21,7 @@ impl SourceOperator {
     }
 }
 
-impl Source<(), usize> for SourceOperator {
+impl Source<usize> for SourceOperator {
     fn run(&mut self, _operator_config: &OperatorConfig, write_stream: &mut WriteStream<usize>) {
         tracing::info!("Running Source Operator");
         for t in 0..10 {
@@ -48,52 +49,27 @@ impl SinkOperator {
     }
 }
 
-struct SinkOperatorState {
-    message_counter: HashMap<Timestamp, usize>,
-    current_timestamp: Timestamp,
-}
-
-impl SinkOperatorState {
-    fn new() -> Self {
-        Self {
-            message_counter: HashMap::new(),
-            current_timestamp: Timestamp::Bottom,
-        }
-    }
-
-    fn increment_message_count(&mut self, timestamp: &Timestamp) {
-        let count = self.message_counter.entry(timestamp.clone()).or_insert(0);
-        *count += 1;
-    }
-
-    fn get_message_count(&self, timestamp: &Timestamp) -> usize {
-        *self.message_counter.get(timestamp).unwrap_or_else(|| &0)
-    }
-}
-
-impl StateT for SinkOperatorState {
-    fn commit(&mut self, timestamp: &Timestamp) {
-        self.current_timestamp = timestamp.clone();
-    }
-
-    fn get_last_committed_timestamp(&self) -> Timestamp {
-        self.current_timestamp.clone()
-    }
-}
-
-impl Sink<SinkOperatorState, usize> for SinkOperator {
-    fn on_data(&mut self, ctx: &mut SinkContext<SinkOperatorState>, data: &usize) {
-        let timestamp = ctx.get_timestamp().clone();
-        tracing::info!("SinkOperator @ {:?}: Received {}", timestamp, data);
-        ctx.get_state().increment_message_count(&timestamp);
-    }
-
-    fn on_watermark(&mut self, ctx: &mut SinkContext<SinkOperatorState>) {
+impl Sink<TimeVersionedState<usize>, usize> for SinkOperator {
+    fn on_data(&mut self, ctx: &mut SinkContext<TimeVersionedState<usize>>, data: &usize) {
         let timestamp = ctx.get_timestamp().clone();
         tracing::info!(
-            "SinkOperator @ {:?}: Received {} data messages.",
+            "{} @ {:?}: Received {}",
+            ctx.get_operator_config().get_name(),
             timestamp,
-            ctx.get_state().get_message_count(&timestamp),
+            data,
+        );
+
+        // Increment the message count.
+        *ctx.get_current_state().unwrap() += 1;
+    }
+
+    fn on_watermark(&mut self, ctx: &mut SinkContext<TimeVersionedState<usize>>) {
+        let timestamp = ctx.get_timestamp().clone();
+        tracing::info!(
+            "{} @ {:?}: Received {} data messages.",
+            ctx.get_operator_config().get_name(),
+            timestamp,
+            *ctx.get_current_state().unwrap(),
         );
     }
 }
@@ -103,7 +79,7 @@ fn main() {
     let mut node = Node::new(Configuration::from_args(&args));
 
     let source_config = OperatorConfig::new().name("SourceOperator");
-    let source_stream = erdos::connect_source(SourceOperator::new, || {}, source_config);
+    let source_stream = erdos::connect_source(SourceOperator::new, source_config);
 
     let (split_stream_less_50, split_stream_greater_50) = source_stream
         .map(|x: &usize| -> usize { 2 * x })
@@ -113,7 +89,7 @@ fn main() {
     let left_sink_config = OperatorConfig::new().name("LeftSinkOperator");
     erdos::connect_sink(
         SinkOperator::new,
-        SinkOperatorState::new,
+        TimeVersionedState::new,
         left_sink_config,
         &split_stream_less_50,
     );
@@ -121,7 +97,7 @@ fn main() {
     let right_sink_config = OperatorConfig::new().name("RightSinkOperator");
     erdos::connect_sink(
         SinkOperator::new,
-        SinkOperatorState::new,
+        TimeVersionedState::new,
         right_sink_config,
         &split_stream_greater_50,
     );
