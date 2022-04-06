b262d258f733d44db72065ff8ff6d659e924effc
Peter Schafhalter <pschafhalter@berkeley.edu>
2022-04-06 17:55:50 +0200

    Bump to version 0.4.0

--- python/doc/source/conf.py
+++ python/doc/source/conf.py
@@ -24,9 +24,9 @@ copyright = "2018, The ERDOS Team"
 author = "The ERDOS Team"
 
 # The short X.Y version
-version = "0.3"
+version = "0.4"
 # The full version, including alpha/beta/rc tags
-release = "0.3.1"
+release = "0.4.0"
 
 # -- General configuration ---------------------------------------------------
 
