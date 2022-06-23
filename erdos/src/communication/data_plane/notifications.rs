81d719b866a50f7e2031c504bddb12421d6d5e61
Sukrit Kalra <sukritkalra@gmail.com>
2022-06-22 23:32:03 -0700

    Create DataSenders and DataReceivers for each Worker connection.

--- erdos/src/communication/data_plane/notifications.rs
+++ erdos/src/communication/data_plane/notifications.rs
@@ -1,10 +1,15 @@
 use std::collections::HashMap;
 
 use crate::{
-    communication::control_plane::notifications::WorkerAddress,
+    communication::{control_plane::notifications::WorkerAddress, PusherT},
     dataflow::{graph::AbstractOperator, stream::StreamId},
 };
 
+#[derive(Clone)]
 pub(crate) enum DataPlaneNotification {
     SetupConnections(AbstractOperator, HashMap<StreamId, WorkerAddress>),
+    ReceiverInitialized(usize),
+    SenderInitialized(usize),
+    PusherUpdate(StreamId, Box<dyn PusherT>),
+    PusherUpdated(StreamId),
 }
