a2a465ed7e240215933cb660a6ee788cd5dfa03a
Sukrit Kalra <sukritkalra@gmail.com>
2022-07-01 13:18:33 -0700

    Assign random WorkerId if the requested Worker was not found.

--- erdos/src/scheduler/mod.rs
+++ erdos/src/scheduler/mod.rs
@@ -1,4 +1,6 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+
+use rand::{thread_rng, Rng};
 
 use crate::{
     dataflow::graph::{AbstractJobGraph, Job},
@@ -29,13 +31,31 @@ impl JobGraphScheduler for SimpleJobGraphScheduler {
     fn schedule_graph(
         &mut self,
         job_graph: &AbstractJobGraph,
-        _workers: &Vec<WorkerState>,
+        workers: &Vec<WorkerState>,
     ) -> HashMap<Job, WorkerId> {
+        if workers.len() == 0 {
+            // No workers found, we cannot schedule anything.
+            return HashMap::new();
+        }
+
+        // Calculate the requested placement, and assign a random Worker if the
+        // requested Worker was not found.
         let mut placements = HashMap::new();
+        let worker_ids: HashSet<_> = workers
+            .iter()
+            .map(|worker_state| worker_state.id())
+            .collect();
 
         for (operator_id, operator) in job_graph.operators().iter() {
             let requested_worker = operator.config.worker_id;
-            placements.insert(operator_id.clone(), requested_worker);
+            if worker_ids.contains(&requested_worker) {
+                placements.insert(operator_id.clone(), requested_worker);
+            } else {
+                // Assign a random worker.
+                let worker_index = thread_rng().gen_range(0, worker_ids.len());
+                let worker_id = workers.get(worker_index).unwrap().id();
+                placements.insert(operator_id.clone(), worker_id);
+            }
         }
 
         placements
